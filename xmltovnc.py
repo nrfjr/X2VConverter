1f7277479f30f37e808e05a16d4ac21dc6120b65
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-12-06 13:16:53 +0800

    Added main script

---
+++ xmltovnc.py
@@ -0,0 +1,43 @@
+from xml.dom import minidom
+
+def generate_vnc_files(file):
+
+    XMLfile = minidom.parse(file, save_dir)
+
+    for i in XMLfile.getElementsByTagName("file"):
+
+        content = []
+        save_file = open(f'{(save_dir)}{"" if save_dir[-1]!="/" else "/"}{i.getAttribute("name")}.vnc')
+
+        content.append(f'ConnMethod=tcp\n')
+        content.append(f'FriendlyName={i.getAttribute("name")}\n')
+        content.append(f'Host={i.getAttribute("name")}\n')
+
+        for j in i.getElementsByTagName("section"):
+
+            for k in j.getElementsByTagName("param"):
+
+                if k.getAttribute("name") is "Identity":
+                    content.append(f'Identity={k.getAttribute("value")}\n')
+                elif k.getAttribute("name") is "Password":
+                    content.append(f'Password={k.getAttribute("value")}\n')
+        
+        content.pop()
+
+def main():
+
+    if len(sys.argv) > 3:
+        printf(f'Too many arguments')
+    elif len(sys.argv) == 2:
+        print(f'Too few argument.')
+    elif len(sys.argv) == 1 or len(sys.argv) == 3:
+        file = sys.argv[1] if sys.argv[1]  else input("Enter RealVNC's xml file: ")
+        save_dir = sys.argv[2] if sys.argv[2] input("Enter saving directory: ")
+
+    generate_vnc_files(file, save_dir)
+
+main()
+    
+ 
+
+
